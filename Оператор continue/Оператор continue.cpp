5e54b7c2310124cb068e927144386ae295dde62d
WaiperOK <ruslan.topol2@gmail.com>
2022-07-02 19:20:36 +0300

    Добавьте файлы проекта.

---
+++ Оператор continue/Оператор continue.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+
+	cout << " начало цикла " << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (i == 2)
+		{
+			continue; // Код не будет выполнятся далее, но будет так же и выход из цикла(Начнется новая иттерация цикла)
+		}
+		cout << " переменная i = " << i << endl;
+	}
+}
